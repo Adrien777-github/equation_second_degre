65ca6ea7c353e8f12249ed8fbec19e86553b4a0e
Adrien777-github <adrienraherinandrasana@gmail.com>
2025-02-26 13:35:24 +0100

    commentaire

--- traitement.php
+++ traitement.php
@@ -5,12 +5,12 @@ function equation_2_degre($a, $b, $c) {
         return "Ce n'est pas une équation quadratique (a ne peut pas être 0).";
     }
     
-    $delta = ($b * $b) - (4 * $a * $c);
+    $delta = ($b * $b) - (4 * $a * $c); // ito ny definition delta
     
     if ($delta > 0) {
-        $x1 = (-$b + sqrt($delta)) / (2 * $a);
+        $x1 = (-$b + sqrt($delta)) / (2 * $a); // ny fonction sqrt(variable) dia racine carré
         $x2 = (-$b - sqrt($delta)) / (2 * $a);
-        return "Deux solutions réelles: x1 = " . round($x1, 2) . "et x2 = " . round($x2, 2);
+        return "Deux solutions réelles: x1 = " . round($x1, 2) . "et x2 = " . round($x2, 2); // round(variable, chiffre après virgule) : arrondissement io fonction io
     } elseif ($delta == 0) {
         $x = -$b / (2 * $a);
         return "Une solution réelle unique: x = " . round($x, 2);
@@ -21,13 +21,14 @@ function equation_2_degre($a, $b, $c) {
 
 $resultat = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $a = isset($_POST['a']) ? floatval($_POST['a']) : 0;
+    // ireto no antsoina hoe condition ternaire
+    $a = isset($_POST['a']) ? floatval($_POST['a']) : 0; 
     $b = isset($_POST['b']) ? floatval($_POST['b']) : 0;
     $c = isset($_POST['c']) ? floatval($_POST['c']) : 0;
     $resultat = equation_2_degre($a, $b, $c);
 }
 
-// Stocker les variables dans la session
+// Stocker les variables dans la session (afaka à passe passe par fichier ny variable session)
 $_SESSION['resultat'] = $resultat;
 
 // Redirection
